c604d44d192b7790835f65f0ce628785dc806e23
dnhmd <81837784+dnhmd@users.noreply.github.com>
2026-07-25 15:45:34 +0300

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,9 @@
+# Using Hashmaps
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        freq = {}
+        for num in nums:
+            if num in freq:
+                return True
+            freq[num] = 1
+        return False
