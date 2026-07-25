630e1cc752c414cd8ab687bd68485bcf9d99d8c5
dnhmd <81837784+dnhmd@users.noreply.github.com>
2026-07-25 17:12:15 +0300

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,19 @@
+
+# Using HashMaps
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        store = [0] * 26
+
+        for i in range(len(s)):
+            store[ord(s[i]) - ord('a')] += 1
+            store[ord(t[i]) - ord('a')] -= 1
+        
+        for num in store:
+            if num > 0:
+                return False
+
+        return True
+        
